049a35c38239dac4eeb7fbcfadc18ced93bf05b5
codingforheaven <iakarshfans@gmail.com>
2016-09-14 22:25:15 +0530

    Create car-type.php

---
+++ car-type.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="apple-touch-icon" href="apple-touch-icon.png">
+	<title>PickMyCar</title>
+	
+	<!--animate CSS-->
+	<link rel="stylesheet" href="assets/css/animate.css">
+	<!--Bootstrap CSS-->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<!--Main Stylesheet-->
+	<link rel="stylesheet" href="assets/css/core.css">
+	<!--Responsive CSS-->
+	<link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+       
+<body>
+   <div class="fixed-layout">
+  		
+   		<div class="container-fluid">
+   			<div class="row">
+   				<div class="col-sm-12 logo">
+   					<a href="index.php"><img src="img/logo.png" alt=""></a>
+   				</div><!--END col-sm-12 logo-->
+   			</div><!--END row-->
+   		</div><!--END container-fluid-->
+   		
+   		<div class="container-fluid">
+   			<div class="row">
+   				<div class="col-sm-12 text-center page-title">
+   					<h2>Which car type do you like?</h2>
+   				</div><!--END col-sm-12 text-center page-title-->
+   			</div><!--END row-->
+   		</div><!--END container-fluid-->
+   		
+   		<div class="container-fluid">
+   			<div class="row">
+				<div class="col-md-3 col-sm-6 text-center choice-box single-car">
+					<a href="budget.php?type=1">
+						<img src="img/car/hatchback.png" alt="">
+						<h3>Hatchback</h3>
+					</a>
+				</div><!--END choice-box single-car-->
+				<div class="col-md-3 col-sm-6 text-center choice-box single-car">
+					<a href="budget.php?type=2">
+						<img src="img/car/sedan.png" alt="">
+						<h3>Sedan</h3>
+					</a>
+				</div><!--END choice-box single-car-->
+				<div class="col-md-3 col-sm-6 text-center choice-box single-car">
+					<a href="budget.php?type=3">
+						<img src="img/car/suv.png" alt="">
+						<h3>SUV</h3>
+					</a>
+				</div><!--END choice-box single-car-->
+				<div class="col-md-3 col-sm-6 text-center choice-box single-car">
+					<a href="budget.php?type=4">
+						<img src="img/car/minivan.png" alt="">
+						<h3>MUV</h3>
+					</a>
+				</div><!--END choice-box single-car-->
+   			</div><!--END row-->
+   		</div><!--END container-fluid-->
+   		
+   </div><!--END fixed-layout-->
+
+ 
+
+
+		<!-- jQuery core -->
+        <script src="assets/js/jquery.min.js"></script>
+        <!--Bootrstrap JS-->
+        <script src="assets/js/bootstrap.min.js"></script>
+        <!--wow JS-->
+        <script src="assets/js/wow.min.js"></script>
+        <!--Main JS-->
+        <script src="assets/js/main.js"></script>
+</body>
+</html>
